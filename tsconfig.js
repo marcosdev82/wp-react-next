f6505f44965fabe9ef33b858117e33c0fcf3b425
mikek182 <marcosvinicius@geekwork.com.br>
2025-09-29 14:24:10 -0300

    Adicionar Slider com Swiper e integração do Radix UI

--- tsconfig.js
+++ tsconfig.js
@@ -2,9 +2,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",  // certifique-se de que aponta para seus arquivos
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}" // <- importante
   ],
   theme: {
     extend: {
