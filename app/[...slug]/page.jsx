c7032b576b638e7c386e1f1827719f168cf8379e
mikek182 <marcosvinicius@geekwork.com.br>
2025-10-03 14:13:38 -0300

    Componente BrandCrumb

--- app/[...slug]/page.jsx
+++ app/[...slug]/page.jsx
@@ -1,31 +1,65 @@
-// app/[slug]/page.js
-// import { Container } from "lucide-react";
+// app/[...slug]/page.js
 import { notFound } from "next/navigation";
 import EntryTitle from "../components/EntryTitle";
-import Container from "../components/Container"
+import Container from "../components/Container";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 
-// Simulando busca do banco / CMS
-async function getPage(slug) {
+// Simulando busca no "CMS"
+async function getPage(slugArray) {
+    const slugPath = slugArray.join("/");
+
+    // https://sbot.org.br/wp-json/wp/v2/pages
+
     const pages = {
         "sobre": { title: "Sobre nós", content: "Texto da página sobre..." },
-        "associados": { title: "Associados", content: "Texto da página associados..." }
+        "associados": { title: "Associados", content: "Texto da página associados..." },
+        "associados/beneficios": { title: "Benefícios", content: "Página filha de associados" },
+        "associados/beneficios/descontos": { title: "Descontos", content: "Página neta de associados" },
     };
 
-    return pages[slug] || null;
+    return pages[slugPath] || null;
 }
 
 export default async function Page({ params }) {
-    const page = await getPage(params.slug);
+    const { slug = [] } = params; // slug vira array
+    const page = await getPage(slug);
 
     if (!page) return notFound();
 
     return (
         <article>
-            <EntryTitle>{page.title}</EntryTitle>
+            <EntryTitle>
+                <h1>{page.title}</h1> 
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                        <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                        <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </EntryTitle>
             <Container>
                 <div className="content py-6 px-3">
                     {page.content}
                 </div>
+                <small className="block mt-4 text-gray-500">
+                    Caminho atual: /{slug.join("/")}
+                </small>
             </Container>
         </article>
     );
--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -5,7 +5,9 @@ export default function Sobre() {
     return (
 
         <article>
-            <EntryTitle>Teste</EntryTitle>
+            <EntryTitle>
+                <h1>teste</h1>
+            </EntryTitle>
             <Container>
                 <div className="content py-6 px-3">
                     Meu conteúdo aqui
