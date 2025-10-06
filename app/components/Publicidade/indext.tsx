38db373fe170309016ccce91a1db01fb37d272d7
mikek182 <marcosvinicius@geekwork.com.br>
2025-10-06 13:35:27 -0300

    Imagem do componente conteúdo pratrocinado

--- app/components/Publicidade/indext.tsx
+++ app/components/Publicidade/indext.tsx
@@ -1,5 +1,23 @@
+'use client'
+
+import Image from "next/image"
+import Link from "next/link"
+import Container from "../Container"
+
 export default function Publicidade() {
     return (
-        <h1>Mini Banner aqui</h1>
+        <section className="flex items-center justify-center h-60 px-3">
+            <Container>
+                <h1>Conteúdos Patrocinados</h1>
+                <Link href="/">
+                    <Image
+                        src="/Banner-portal-SBOT-600x197.jpg" // <-- use o path direto da pasta public
+                        alt="Conteúdos Patrocinados"
+                        width={600}
+                        height={197}
+                    />
+                </Link>
+            </Container>
+        </section>
     )
 }
