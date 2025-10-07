821a4a9039e1bb28ae173c9ef436ffbad51fc5d5
mikek182 <marcosvinicius@geekwork.com.br>
2025-10-07 17:39:36 -0300

    hook bread crumb

--- app/[...slug]/page.jsx
+++ app/[...slug]/page.jsx
@@ -1,21 +1,11 @@
-// app/[...slug]/page.js
 import { notFound } from "next/navigation";
 import EntryTitle from "../components/EntryTitle";
 import Container from "../components/Container";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+import BreadcrumbDynamic from "../components/BreadcrumbDynamic";
 
-// Simulando busca no "CMS"
+// Simulando busca no CMS
 async function getPage(slugArray) {
     const slugPath = slugArray.join("/");
-
-    // https://sbot.org.br/wp-json/wp/v2/pages
 
     const pages = {
         "sobre": { title: "Sobre nós", content: "Texto da página sobre..." },
@@ -25,42 +15,27 @@ async function getPage(slugArray) {
     };
 
     return pages[slugPath] || null;
-}
+    }
 
-export default async function Page({ params }) {
-    const { slug = [] } = params; // slug vira array
+    export default async function Page({ params }) {
+    const { slug = [] } = params;
     const page = await getPage(slug);
 
     if (!page) return notFound();
 
     return (
         <article>
-            <EntryTitle>
-                <h1>{page.title}</h1> 
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem>
-                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator />
-                        <BreadcrumbItem>
-                        <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator />
-                        <BreadcrumbItem>
-                        <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </EntryTitle>
-            <Container>
-                <div className="content py-6 px-3">
-                    {page.content}
-                </div>
-                <small className="block mt-4 text-gray-500">
-                    Caminho atual: /{slug.join("/")}
-                </small>
-            </Container>
+        <EntryTitle>
+            <h1>{page.title}</h1>
+            {/* Breadcrumb é um componente Client */}
+            <BreadcrumbDynamic slug={slug} />
+        </EntryTitle>
+
+        <Container>
+            <div className="content py-6 px-3">
+            {page.content}
+            </div>
+        </Container>
         </article>
     );
 }
